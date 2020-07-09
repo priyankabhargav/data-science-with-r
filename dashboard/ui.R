52ccb20f8949e0507d46d41af7b1b6dcce82d843
Nandish <nandish21594@gmail.com>
2020-07-09 02:04:26 +0200

    Removed the process notebook as it was slowing and crashing the app due to it's huge size

--- dashboard/ui.R
+++ dashboard/ui.R
@@ -13,18 +13,13 @@ library(reshape2)
 library(rbokeh)
 library(dashboardthemes)
 
-# load(file = "top_10_model.Rdata")
-
 #UI elements
 ui<-
   dashboardPage( 
     
-    
-    # skin = "purple", 
-                 #“blue”, “black”, “purple”, “green”, “red”, “yellow”, "blue_gradient"
                  dashboardHeader(title =  shinyDashboardLogo(
                    theme = "purple_gradient",
-                   boldText = "Classification of Covid-19 news articles",
+                   boldText = "Multi-label Classification of News Articles on Coronavirus",
                    mainText = "",
                    badgeText = "Data Science with R"
                  )),
@@ -34,24 +29,18 @@ ui<-
                    tags$head(
                      tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
                    ),
-                   # title = "Data Science with R",
-                   
                    sidebarMenu(
                      menuItem("Overview and Motivation", tabName = "overview", icon = icon("th")),
-                     menuItem("Related Work", tabName = "related_work", icon = icon("hands-helping")),
-                     menuItem("Objective", tabName = "objective", icon = icon("bullseye")),
-                     # menuItem("The Team", tabName = "team", icon = icon("users")),
+                     menuItem("Objective and Related Work", tabName = "related_work", icon = icon("hands-helping")),
+                     # menuItem("Objective", tabName = "objective", icon = icon("bullseye")),
                      menuItem("Data", tabName = "data", icon = icon("database")),
                      menuItem("Exploratory Analysis", tabName = "exploratory_analysis", icon = icon("table")),
                      menuItem("Demo", tabName = "demo", icon = icon("bar-chart-o")
-                              # menuSubItem("AP(Mill) Mumbai-MH",
-                              #             tabName = "dashboard3")
                               ,menuSubItem("LDA Visualization",tabName = "ldaVis_dashboard")
                               ,menuSubItem("Topic-Terms Plot",tabName = "termTopic_dashboard")
                               ,menuSubItem("Bokeh Plot",tabName = "bokeh_dashboard")
                               ,menuSubItem("Prediction", tabName = "prediction")
                      ),
-                     # menuItem("Prediction", tabName = "prediction", icon = icon("chart-bar")),
                      menuItem("Final Analysis", tabName = "final_analysis", icon = icon("list-alt")),
                      menuItem("Process Notebook", tabName = "markdown", icon = icon("file-alt")),
                      menuItem(" Repository", tabName = "repository", icon = icon("code-branch")),
@@ -74,32 +63,33 @@ ui<-
                              h1("Overview and Motivation",align="center"),
                              fluidRow(column(12, wellPanel(includeText("text content/text.txt")))),
                              box(align="center", width = "100%",
-                                 tags$iframe(width="100%", height= 425, src="https://www.youtube.com/embed/T1-k7VYwsHg", frameborder="2", allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture", allowfullscreen=TRUE)
-                             )
+                                 tags$iframe(width="100%", height= 425, src="https://www.youtube.com/embed/z9FDROKYoDw", frameborder="2", allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture", allowfullscreen=TRUE)
+                             ),
+                             fluidRow(column(12, wellPanel(includeText("text content/text_2.txt"))))
                      ),
                      
                      # Related Work
                      tabItem(tabName = "related_work",
-                             h1("Related Work", align="center"),
+                             h1("Objective and Related Work", align="center"),
+                             htmlOutput("objectivePage"),
                              htmlOutput("relatedPage")
-                             
                      ),
                      
                      # Objective
-                     tabItem(tabName = "objective",
-                             h1("Objective", align="center"),
-                             htmlOutput("objectivePage")
-                             
-                     ),
+                     # tabItem(tabName = "objective",
+                     #         h1("Objective", align="center"),
+                     #         htmlOutput("objectivePage")
+                     #         
+                     # ),
                      
                      # Data
                      tabItem(tabName = "data",
-                             h1("Data", align="center"),
+                             # h1("Data", align="center"),
                              htmlOutput("dataPage")
                      ),
                      #Exploratory Analysis
                      tabItem(tabName = "exploratory_analysis",
-                             h1("Exploratory Analysis", align="center"),
+                             # h1("Exploratory Analysis", align="center"),
                              htmlOutput("exploPage")
                      ),
                      
@@ -156,21 +146,29 @@ ui<-
                      
                      # Process Markdown
                      tabItem(tabName = "final_analysis",
-                             h1("Final Analysis", align="center"),
+                             # h1("Final Analysis", align="center"),
                              htmlOutput("finalPage")
-                             
-
                      ),
                      
                      # Process Markdown
+                     # tabItem(tabName = "markdown",
+                     #         # h1("Process Notebook", align="center"),
+                     #         fluidPage(
+                     #           mainPanel(
+                     #             htmlOutput("proPage")
+                     #           )
+                     #         )
+                     # ),
+                     
+                     # Repository
                      tabItem(tabName = "markdown",
                              h1("Process Notebook", align="center"),
-                             fluidPage(
-                               mainPanel(
-                                 htmlOutput("proPage")
-                               )
+                             fluidRow(column(12, wellPanel(includeText("text content/text_3.txt")))),
+                             box(align="center", width = "10%", height="10%",
+                                 tags$a(img(src="https://ikuyadeu.gallerycdn.vsassets.io/extensions/ikuyadeu/r/1.4.2/1592286900390/Microsoft.VisualStudio.Services.Icons.Default",
+                                            height="35%", width="35%", align="center"), 
+                                        href="https://drive.google.com/drive/folders/1k5OCaLC_VfymXlpt9z48y5bOrwvLkf6z")
                              )
-                             
                      ),
                      
                      # Repository
@@ -182,15 +180,12 @@ ui<-
                                             height="50%", width="50%", align="center"), 
                                         href="https://github.com/CalidaPereira/data-science-with-r")
                              )
-                             
                      ),
                      
                      # References
                      tabItem(tabName = "references",
                              h1("References", align="center"),
-                             htmlOutput("referencePage")
-                             
-                             
+                             htmlOutput("referencePage")  
                      )
                    )
                  )
